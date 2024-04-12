0ae00f6cc2fb04b41081c4b6c4cca52f4a83a16c
moaoa <moaadbn3@gmail.com>
2024-04-12 23:27:06 +0200

    feature: handle user agent header

--- app/main.js
+++ app/main.js
@@ -12,10 +12,32 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const requestLine = data.toString().split("\r\n")[0];
     const [requestMethod, path, httpVersion] = requestLine.split(" ");
+
+    const requestParts = data.toString().split("\r\n");
+
+    const headers = {};
+
+    let i = 1;
+
+    while (requestParts[i] !== "") {
+      const keyValuePairs = requestParts[i].split(":");
+      const key = keyValuePairs[0];
+      const value = keyValuePairs[1];
+      headers[key] = value;
+      i++;
+    }
+
     if (path === "/") {
       socket.write("HTTP/1.1  200 OK\r\n\r\n", console.error);
     } else if (path.startsWith("/echo")) {
       const data = path.slice("/echo/".length);
+
+      socket.write("HTTP/1.1  200 OK\r\n");
+      socket.write("Content-Type: text/plain\r\n");
+      socket.write(`Content-Length: ${data.length}\r\n\r\n`);
+      socket.write(data);
+    } else if (path.startsWith("/user-agent")) {
+      const data = path.replace("/user-agent/", "");
 
       socket.write("HTTP/1.1  200 OK\r\n");
       socket.write("Content-Type: text/plain\r\n");
